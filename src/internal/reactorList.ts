cba36795a5f0a4660df1e373dc209172ddcc4003
Tristan Guichaoua <baanlohdev@gmail.com>
2020-04-29 18:16:49 +0200

    fix list toString issue

--- src/internal/reactorList.ts
+++ src/internal/reactorList.ts
@@ -3,7 +3,7 @@ import { ReactorOptions, reactor, OnCollectParams } from "./reactor";
 import { sendListMessage } from "./sendListMessage";
 import indexed_emojis from "../indexed-emojis";
 
-export type ReactorListOptions<T> = ReactorOptions & { toString?: (o: T) => string };
+export type ReactorListOptions<T> = ReactorOptions & { stringify?: (o: T) => string };
 
 export async function reactorList<T>(
     channel: TextBasedChannelFields,
@@ -15,7 +15,7 @@ export async function reactorList<T>(
     options?: ReactorListOptions<T>
 ) {
     if (list.length === 0) return null;
-    const message = await sendListMessage(channel, prompt, list, options?.toString);
+    const message = await sendListMessage(channel, prompt, list, options?.stringify);
     const emojis = indexed_emojis.slice(0, list.length);
 
     return reactor<T | null>(
