5ee585d59240f2104512a43a68156f26f4d9b552
Tristan Guichaoua <baanlohdev@gmail.com>
2020-04-29 19:26:07 +0200

    fix doc

--- src/functions/confirm.ts
+++ src/functions/confirm.ts
@@ -6,7 +6,7 @@ import { ReactorOptions, reactor, UserFilter } from "../internal/reactor";
  * The returned promised is resolve when user click on one of the added reaction.
  * The resolved value is true if the user click on the check mark reaction, false otherwise.
  * @param message - The message that received reactions.
- * @param userFilter s
+ * @param userFilter - Determin if a user is allow to react.
  * @param options 
  */
 export async function confirm(message: Message, userFilter?: UserFilter, options?: ReactorOptions) {
