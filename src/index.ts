0436e1d486d27bc8cf781119686484b92d314687
Tristan Guichaoua <baanlohdev@gmail.com>
2020-05-09 16:26:19 +0200

    export VoteResult interface

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { confirm } from "./functions/confirm";
 export { unanimousVote } from "./functions/unanimousVote";
 export { vote } from "./functions/vote";
+
+export { VoteResult } from "./internal/reactorVote";
