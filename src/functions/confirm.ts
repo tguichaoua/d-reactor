ee0a380ac1409502343bb55b18b205f0d994f77e
Tristan Guichaoua <baanlohdev@gmail.com>
2020-06-28 18:53:19 +0200

    ⚡️ update confirm

--- src/functions/confirm.ts
+++ src/functions/confirm.ts
@@ -1,25 +1,44 @@
-import { TextBasedChannelFields } from "discord.js";
+import { TextBasedChannelFields, User } from "discord.js";
 import emojis from "../misc/emojis.json";
-import { ReactorOptions, UserFilter } from "../internal/reactor";
-import { reactorMessage } from "../internal/reactorMessage";
+import { ReactorOptions } from "../internal/reactor";
+import { Reactor } from "../models/Reactor";
+import { Predicate } from "../models/Predicate";
 
 /**
  * The returned promised is resolve when user click on one of the added reaction.
  * The resolved value is true if the user click on ✅, false otherwise.
- * @param channel - Channel where the message is post.
- * @param caption - Message caption.
- * @param userFilter - Determines if a user is allow to react.
+ * @param channel Channel where the message is post.
+ * @param caption Message caption.
+ * @param userFilter Determines if a user is allow to react.
  * @param options 
  */
-export function confirm(channel: TextBasedChannelFields, caption: string, userFilter?: UserFilter, options?: ReactorOptions) {
-    return reactorMessage<boolean>(
-        channel,
-        caption,
+// export function confirm(channel: TextBasedChannelFields, caption: string, userFilter?: UserFilter, options?: ReactorOptions) {
+//     return reactorMessage<boolean>(
+//         channel,
+//         caption,
+//         [emojis.checkMark, emojis.crossMark],
+//         () => false,
+//         ({ reaction }) => { return { value: reaction.emoji.name === emojis.checkMark } },
+//         undefined,
+//         userFilter,
+//         options
+//     );
+// }
+
+export function confirm(
+    channel: TextBasedChannelFields,
+    caption: string,
+    userFilter?: Predicate<User>,
+    options?: ReactorOptions
+) {
+    return new Reactor<boolean, false>(
+        channel.send(caption),
         [emojis.checkMark, emojis.crossMark],
         () => false,
+        undefined,
         ({ reaction }) => { return { value: reaction.emoji.name === emojis.checkMark } },
         undefined,
         userFilter,
-        options
+        options,
     );
 }
