8292ab205aa7e0598973f04ed4b8fd9a61ff367e
Tristan Guichaoua <baanlohdev@gmail.com>
2020-06-28 19:26:51 +0200

    ♻️ refactoring internal list related methods.

--- src/internal/reactorList.ts
+++ src/internal/reactorList.ts
@@ -1,60 +1,100 @@
 import { TextBasedChannelFields, ReactionCollector, EmojiResolvable } from "discord.js";
-import { ReactorOptions, reactor, OnReactionChangedParams, UserFilter } from "./reactor";
-import { sendListMessage, MessageListOptions } from "./sendListMessage";
-import { makeCancellable } from "../misc/makeCancellable";
+import { ReactorOptions, OnReactionChangedParams, UserFilter } from "./reactor";
+import { makeListMessage } from "./sendListMessage";
+import { Reactor } from "../models/Reactor";
+import { MessageListOptions } from "../models/options/MessageListOptions";
 
-export interface Button<R> {
+export interface ListButton<R> {
     emoji: EmojiResolvable;
     action: () => { value: R } | void;
 }
 
-export type ListOptions<T> = ReactorOptions & MessageListOptions<T>;
+export type ListOptions<T> = ReactorOptions & Partial<MessageListOptions<T>>;
 
-/** @internal */
-export function reactorList<T, R>(
+// /** @internal */
+// export function reactorList<T, R>(
+//     channel: TextBasedChannelFields,
+//     caption: string,
+//     list: readonly T[],
+//     onEnd?: (collector: ReactionCollector) => R,
+//     onCollect?: (params: OnReactionChangedParams & { readonly index: number }) => { value: R } | boolean | void,
+//     onRemove?: (params: OnReactionChangedParams & { readonly index: number }) => void,
+//     userFilter?: UserFilter,
+//     options?: ListOptions<T>,
+//     buttons: Button<R>[] = [],
+// ) {
+//     return makeCancellable<R>(
+//         async onCancel => {
+//             onCancel.shouldReject = false;
+
+//             if (list.length === 0)
+//                 throw new Error("List must not be empty.");
+
+//             const { message, emojis } = await sendListMessage(channel, caption, list, options);
+//             const buttonEmojis = buttons.map(b => b.emoji);
+
+//             const promise = reactor<R>(
+//                 message,
+//                 [...emojis, ...buttonEmojis],
+//                 onEnd,
+//                 (params) => {
+//                     const btnIndex = buttonEmojis.indexOf(params.reaction.emoji.name);
+//                     if (btnIndex !== -1) {
+//                         return buttons[btnIndex].action() ?? false;
+//                     } else {
+//                         return onCollect ?
+//                             onCollect(Object.assign(params, { index: emojis.indexOf(params.reaction.emoji.name) }))
+//                             : undefined
+//                     }
+//                 },
+//                 onRemove ?
+//                     (params) => onRemove(Object.assign(params, { index: emojis.indexOf(params.reaction.emoji.name) }))
+//                     : undefined,
+//                 userFilter,
+//                 options
+//             );
+
+//             onCancel(() => promise.cancel());
+//             return promise;
+//         }
+//     );
+// }
+
+export function reactorList<T, R, C = R>(
     channel: TextBasedChannelFields,
     caption: string,
     list: readonly T[],
+    onCancel: (collector: ReactionCollector) => C,
     onEnd?: (collector: ReactionCollector) => R,
     onCollect?: (params: OnReactionChangedParams & { readonly index: number }) => { value: R } | boolean | void,
     onRemove?: (params: OnReactionChangedParams & { readonly index: number }) => void,
     userFilter?: UserFilter,
     options?: ListOptions<T>,
-    buttons: Button<R>[] = [],
+    buttons: ListButton<R>[] = [],
 ) {
-    return makeCancellable<R>(
-        async onCancel => {
-            onCancel.shouldReject = false;
 
-            if (list.length === 0)
-                throw new Error("List must not be empty.");
+    const { message, emojis } = makeListMessage(channel, caption, list, options);
+    const buttonEmojis = buttons.map(b => b.emoji);
 
-            const { message, emojis } = await sendListMessage(channel, caption, list, options);
-            const buttonEmojis = buttons.map(b => b.emoji);
-
-            const promise = reactor<R>(
-                message,
-                [...emojis, ...buttonEmojis],
-                onEnd,
-                (params) => {
-                    const btnIndex = buttonEmojis.indexOf(params.reaction.emoji.name);
-                    if (btnIndex !== -1) {
-                        return buttons[btnIndex].action() ?? false;
-                    } else {
-                        return onCollect ?
-                            onCollect(Object.assign(params, { index: emojis.indexOf(params.reaction.emoji.name) }))
-                            : undefined
-                    }
-                },
-                onRemove ?
-                    (params) => onRemove(Object.assign(params, { index: emojis.indexOf(params.reaction.emoji.name) }))
-                    : undefined,
-                userFilter,
-                options
-            );
-
-            onCancel(() => promise.cancel());
-            return promise;
-        }
+    return new Reactor<R, C>(
+        message,
+        [...emojis, ...buttonEmojis],
+        onCancel,
+        onEnd,
+        (params) => {
+            const btnIndex = buttonEmojis.indexOf(params.reaction.emoji.name);
+            if (btnIndex !== -1) {
+                return buttons[btnIndex].action() ?? false;
+            } else {
+                return onCollect ?
+                    onCollect(Object.assign(params, { index: emojis.indexOf(params.reaction.emoji.name) }))
+                    : undefined
+            }
+        },
+        onRemove ?
+            (params) => onRemove(Object.assign(params, { index: emojis.indexOf(params.reaction.emoji.name) }))
+            : undefined,
+        userFilter,
+        options,
     );
 }
--- src/internal/sendListMessage.ts
+++ src/internal/sendListMessage.ts
@@ -1,42 +1,49 @@
 import { TextBasedChannelFields, EmojiResolvable } from "discord.js";
 import indexed_emojis from "../misc/indexed-emojis.json";
+import { MessageListOptions } from "../models/options/MessageListOptions";
 
-export interface MessageListOptionsFull<T> {
-    /** This function is called for each element in the list to convert them into a string in the message. (default is `o => '${o}'`) */
-    stringify: (o: T) => string,
-    /** This list of emojis is used to determine the emoji that represent each elements of the list.
-     * If the emoji list is shorter than the list of element, default emojis are used.
-     * Warning: no check is done on this list. Passing invalid values can lead to undetermined behaviour.
-     */
-    emojis: EmojiResolvable[],
+// export interface MessageListOptionsFull<T> {
+//     /** This function is called for each element in the list to convert them into a string in the message. (default is `o => '${o}'`) */
+//     stringify: (o: T) => string,
+//     /** This list of emojis is used to determine the emoji that represent each elements of the list.
+//      * If the emoji list is shorter than the list of element, default emojis are used.
+//      * Warning: no check is done on this list. Passing invalid values can lead to undetermined behaviour.
+//      */
+//     emojis: EmojiResolvable[],
 
-};
+// };
 
-export type MessageListOptions<T> = Partial<MessageListOptionsFull<T>>
+// export type MessageListOptions<T> = Partial<MessageListOptionsFull<T>>
+
+// /** @internal */
+// const defautlOptions: MessageListOptionsFull<any> = {
+//     stringify: o => `${o}`,
+//     emojis: [],
+// }
 
 /** @internal */
-const defautlOptions: MessageListOptionsFull<any> = {
-    stringify: o => `${o}`,
-    emojis: [],
-}
-
-/** @internal */
-export async function sendListMessage<T>(
+export function makeListMessage<T>(
     channel: TextBasedChannelFields,
     caption: string,
     list: readonly T[],
-    options?: MessageListOptions<T>
+    options?: Partial<MessageListOptions<T>>
 ) {
     if (list.length > 36)
         throw new Error("The number of elements in list cannot exceed 36.");
 
-    const opts = Object.assign({}, defautlOptions, options);
+    const opts: MessageListOptions<T> = {
+        ...{
+            stringify: o => `${o}`,
+            emojis: []
+        },
+        ...options,
+    }
     const emojis = [...opts.emojis, ...indexed_emojis.slice(opts.emojis.length, list.length)];
 
     let str = `${caption}\n`;
     for (let i = 0; i < list.length; i++)
         str += `${emojis[i]} ${opts.stringify(list[i])}\n`;
 
-    const message = await channel.send(str);
+    const message = channel.send(str);
     return { message, emojis };
 }
