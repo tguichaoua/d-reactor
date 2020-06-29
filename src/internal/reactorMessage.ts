61293d72885b990877e278efd6e8444dc3fc435c
Tristan Guichaoua <baanlohdev@gmail.com>
2020-06-29 09:26:48 +0200

    🔥 comment old code

--- src/internal/reactorMessage.ts
+++ src/internal/reactorMessage.ts
@@ -1,40 +1,40 @@
-import PCancelable from "p-cancelable";
-import { TextBasedChannelFields, EmojiResolvable, ReactionCollector } from "discord.js";
-import { OnReactionChangedParams, UserFilter, ReactorOptions, reactor } from "./reactor";
-import { makeCancellable } from "../misc/makeCancellable";
+// import PCancelable from "p-cancelable";
+// import { TextBasedChannelFields, EmojiResolvable, ReactionCollector } from "discord.js";
+// import { OnReactionChangedParams, UserFilter, ReactorOptions, reactor } from "./reactor";
+// import { makeCancellable } from "../misc/makeCancellable";
 
 
-/** Send message on the channel then use it for reactor.
- * @internal
- */
-export function reactorMessage<T>(
-    channel: TextBasedChannelFields,
-    caption: string,
-    emojis: readonly EmojiResolvable[],
-    onEnd?: (collector: ReactionCollector) => T,
-    onCollect?: (params: OnReactionChangedParams) => { value: T } | boolean | void,
-    onRemove?: (params: OnReactionChangedParams) => void,
-    userFilter?: UserFilter,
-    options?: ReactorOptions
-) {
-    return makeCancellable<T>(
-        async onCancel => {
-            onCancel.shouldReject = false;
+// /** Send message on the channel then use it for reactor.
+//  * @internal
+//  */
+// export function reactorMessage<T>(
+//     channel: TextBasedChannelFields,
+//     caption: string,
+//     emojis: readonly EmojiResolvable[],
+//     onEnd?: (collector: ReactionCollector) => T,
+//     onCollect?: (params: OnReactionChangedParams) => { value: T } | boolean | void,
+//     onRemove?: (params: OnReactionChangedParams) => void,
+//     userFilter?: UserFilter,
+//     options?: ReactorOptions
+// ) {
+//     return makeCancellable<T>(
+//         async onCancel => {
+//             onCancel.shouldReject = false;
 
-            const message = await channel.send(caption);
+//             const message = await channel.send(caption);
 
-            const promise = reactor<T>(
-                message,
-                emojis,
-                onEnd,
-                onCollect,
-                onRemove,
-                userFilter,
-                options
-            );
+//             const promise = reactor<T>(
+//                 message,
+//                 emojis,
+//                 onEnd,
+//                 onCollect,
+//                 onRemove,
+//                 userFilter,
+//                 options
+//             );
             
-            onCancel(() => promise.cancel());
-            return promise;
-        }
-    );
-}
+//             onCancel(() => promise.cancel());
+//             return promise;
+//         }
+//     );
+// }
--- src/misc/makeCancellable.ts
+++ src/misc/makeCancellable.ts
@@ -1,14 +1,14 @@
-import PCancelable, { OnCancelFunction } from "p-cancelable";
+// import PCancelable, { OnCancelFunction } from "p-cancelable";
 
-/** @internal */
-export function makeCancellable<T>(executor: (onCancel: OnCancelFunction) => T | PromiseLike<T>): PCancelable<T> {
-    return new PCancelable<T>(
-        async (resolve, reject, onCancel) => {
-            try {
-                resolve(await executor(onCancel));
-            } catch (error) {
-                reject(error);
-            }
-        }
-    );
-}
+// /** @internal */
+// export function makeCancellable<T>(executor: (onCancel: OnCancelFunction) => T | PromiseLike<T>): PCancelable<T> {
+//     return new PCancelable<T>(
+//         async (resolve, reject, onCancel) => {
+//             try {
+//                 resolve(await executor(onCancel));
+//             } catch (error) {
+//                 reject(error);
+//             }
+//         }
+//     );
+// }
