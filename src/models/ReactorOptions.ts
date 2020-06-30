9f47ae521a45ee4270d33c637003519986f9796a
Tristan Guichaoua <baanlohdev@gmail.com>
2020-06-30 12:56:02 +0200

    📝 update ReactorOptions doc

--- src/models/ReactorOptions.ts
+++ src/models/ReactorOptions.ts
@@ -1,6 +1,6 @@
 export interface ReactorOptions {
-        /** If set, the promise is resolved after this amount of time (in milliseconds) after all reactions being set. */
+        /** If set, the reactor is cancelled with the status `timeout` after this amount of time (in milliseconds) after all reactions being set. */
         duration?: number;
-        /** If set to true, the message is deleted just before the promise is resolved. (default is false) */
+        /** If set to true, the message is deleted just before the reactor value is resolved. (default is false) */
         deleteMessage?: boolean;
 }
