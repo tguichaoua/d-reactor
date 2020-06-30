a7ab9b5bccf3fc1e993e7e667f67d88de6d282f6
Tristan Guichaoua <baanlohdev@gmail.com>
2020-06-30 12:52:16 +0200

    🚚 move MessageListOptions into makeListMessage

--- src/internal/makeListMessage.ts
+++ src/internal/makeListMessage.ts
@@ -1,6 +1,15 @@
-import { TextBasedChannelFields } from "discord.js";
+import { TextBasedChannelFields, EmojiResolvable } from "discord.js";
 import indexed_emojis from "../misc/indexed-emojis.json";
-import { MessageListOptions } from "../models/options/MessageListOptions";
+
+export interface MessageListOptions<T> {
+    /** This function is called for each element in the list to convert them into a string in the message. */
+    stringify: (o: T) => string,
+    /** This list of emojis is used to determine the emoji that represent each elements of the list.
+     * If the emoji list is shorter than the list of element, default emojis are used.
+     * Warning: no check is done on this list. Passing invalid values can lead to undetermined behaviour.
+     */
+    emojis: EmojiResolvable[];
+};
 
 /** @internal */
 export function makeListMessage<T>(
