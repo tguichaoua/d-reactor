6c966714c7f7a70986f5f5f319f58912300e0cf2
Tristan Guichaoua <33934311+baanloh@users.noreply.github.com>
2020-07-02 18:18:05 +0200

    Buttons doc (#14)
    * 📝 doc buttons
    
    * ♻️ fix typo in buttons argument

--- src/functions/buttons.ts
+++ src/functions/buttons.ts
@@ -2,15 +2,31 @@ import { Message, EmojiResolvable, User } from "discord.js";
 import { Reactor } from "../models/Reactor";
 
 interface Button {
+    /** The emoji of the reaction. */
     emoji: EmojiResolvable;
+    /**
+     * A handler that is executed when a user click on the reaction.
+     * @param user the user that click on the reaction.
+     * @param reactor the reactor that handle this button.
+     */
     clicked: (user: User, reactor: Reactor<never, void>) => void | Promise<void>;
 }
 
+/**
+ * Add reactions to the message and execute the action when a user click on the reaction.
+ * 
+ * Resolved value:
+ * - `fulfilled`: never
+ * - `cancelled`: void
+ * 
+ * @param message Message to attach the reaction.
+ * @param buttons A list of buttons.
+ */
 export function buttons(
     message: Message | Promise<Message>,
-    ...butons: Button[]
+    ...buttons: Button[]
 ) {
-    const emojis = butons.map(b => b.emoji);
+    const emojis = buttons.map(b => b.emoji);
     const reactor: Reactor<never, void> = new Reactor<never, void>(
         Promise.resolve(message),
         emojis,
@@ -20,7 +36,7 @@ export function buttons(
             onCollect({ reaction, user }) {
                 const i = emojis.indexOf(reaction.emoji.name);
                 if (i !== -1)
-                    return { remove: true, promise: Promise.resolve(butons[i].clicked(user, reactor)) };
+                    return { remove: true, promise: Promise.resolve(buttons[i].clicked(user, reactor)) };
             }
         }
     );
