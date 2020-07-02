c879329d8f12bad9568a41192336edfd053e3b26
Tristan Guichaoua <baanlohdev@gmail.com>
2020-07-02 18:16:19 +0200

    📝 doc buttons

--- src/functions/buttons.ts
+++ src/functions/buttons.ts
@@ -2,10 +2,26 @@ import { Message, EmojiResolvable, User } from "discord.js";
 import { Reactor } from "../models/Reactor";
 
 interface Button {
+    /** The emoji of the reaction. */
     emoji: EmojiResolvable;
+    /**
+     * A handler that is executed when a user click on the reaction.
+     * @param user the user that click on the reaction.
+     * @param reactor the reactor that handle this button.
+     */
     clicked: (user: User, reactor: Reactor<never, void>) => void | Promise<void>;
 }
 
+/**
+ * Add reactions to the message and execute the action when a user click on the reaction.
+ * 
+ * Resolved value:
+ * - `fulfilled`: never
+ * - `cancelled`: void
+ * 
+ * @param message Message to attach the reaction.
+ * @param butons A list of buttons.
+ */
 export function buttons(
     message: Message | Promise<Message>,
     ...butons: Button[]
